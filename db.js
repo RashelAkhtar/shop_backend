2501e0f018826578602f52f7063126bf11095d07
RashelAkhtar <akhtarrashel408@gmail.com>
2025-08-07 11:01:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,11 +5,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
 
 export default pool;
